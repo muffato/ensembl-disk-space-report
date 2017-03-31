9e22dde00f66af1f8343764967f2947e6355f377
Matthieu Muffato <muffato@ebi.ac.uk>
2017-03-31 14:09:38 +0100

    bugfix: MyISAM and InnoDB stats were inverted

--- servers_disk.R
+++ servers_disk.R
@@ -23,7 +23,7 @@ n_servers = dim(data)[1]
 png(args[2], width=(420+50*n_servers), height=510)
 par(xpd=TRUE, mar=par()$mar+c(4,0,0,5))
 
-b <- barplot(t(as.matrix(data[,(c(3,4,2))])), col=cols, ylab="Disk space in Gb")
+b <- barplot(t(as.matrix(data[,(c(4,3,2))])), col=cols, ylab="Disk space in Gb")
 text(b, par("usr")[2] - 50, xpd=TRUE, srt=45, adj=1, labels=data[,1])
 
 legend('topright', inset=c(-.8/n_servers,0), bty="n", xpd=TRUE, legend=rev(c("MyISAM-used", "InnoDB-used", "Free")), fill=rev(cols))
