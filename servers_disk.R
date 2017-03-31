623ae46adcd11950698ed08c538d4ac15e56344d
Matthieu Muffato <muffato@ebi.ac.uk>
2017-03-31 11:00:04 +0100

    Updated the code for the EBI infrastructure

--- servers_disk.R
+++ servers_disk.R
@@ -2,21 +2,15 @@
 args <- commandArgs(TRUE)
 data <- read.table(args[1], header=TRUE, sep="\t")
 
-data2 <- data
-data2[,2] <- data[,2] - data[,3] - data[,4]
-data2[,3] <- data[,3] - data[,5]
-data2[,c(2,3,4,5)] <- data2[,c(2,3,4,5)]/(1024 * 1024 * 1024)
-
-cols=c("mediumpurple", "blue", "lightblue", "green")
+cols=c("mediumpurple", "blue", "green")
 
 n_servers = dim(data)[1]
-print(dim(data))
-png(args[2], width=(420+50*n_servers), height=480)
-par(xpd=TRUE, mar=par()$mar+c(0,0,0,9))
+png(args[2], width=(420+50*n_servers), height=510)
+par(xpd=TRUE, mar=par()$mar+c(4,0,0,5))
 
-b <- barplot(t(as.matrix(data2[,(c(2,3,5,4))])), col=cols, ylab="Disk space in Gb")
-text(b, par("usr")[2] - 50, xpd=TRUE, srt=45, adj=1, labels=data2[,1])
+b <- barplot(t(as.matrix(data[,(c(3,4,2))])), col=cols, ylab="Disk space in Gb")
+text(b, par("usr")[2] - 50, xpd=TRUE, srt=45, adj=1, labels=data[,1])
 
-legend('topright', inset=c(-.8/n_servers,0), bty="n", xpd=TRUE, legend=rev(c("MyISAM-used", "InnoDB-used", "InnoDB-free", "Free")), fill=rev(cols))
+legend('topright', inset=c(-.8/n_servers,0), bty="n", xpd=TRUE, legend=rev(c("MyISAM-used", "InnoDB-used", "Free")), fill=rev(cols))
 dev.off()
 
--- servers_disk.pl
+++ servers_disk.pl
@@ -5,31 +5,20 @@ use warnings;
 
 use Data::Dumper;
 use DBI;
-
+use File::Basename;
 use JSON qw(decode_json);
+use LWP::Simple;
+
+my $dirname = dirname(__FILE__);
 
 $|++;
-
-my $df_call = 'df -t xfs -P';
 
 my $currTime = localtime();
 
 my $html_pertableinnodb = '
-<li>%s: %d
+<li>%s
  <ul>
    <li>Free       : %.1f</li>
-   <li>InnoDB free: NA</li>
-   <li>InnoDB used: %.1f</li>
-   <li>MyISAM used: %.1f</li>
- </ul>
-</li>
-';
-
-my $html_nopertableinnodb = '
-<li>%s: %d
- <ul>
-   <li>Free       : %.1f</li>
-   <li>InnoDB free: %.1f</li>
    <li>InnoDB used: %.1f</li>
    <li>MyISAM used: %.1f</li>
  </ul>
@@ -51,15 +40,25 @@ my $html_footer = "
 </html>
 ";
 
-my $stm = "SELECT sum(size_in_meg) from (SELECT round(sum(data_length + index_length ) , 2 ) size_in_meg, min(create_time), max(update_time) FROM information_schema.TABLES WHERE ENGINE=(?)   GROUP BY table_schema   ORDER BY size_in_meg DESC) as q;";
+my $url = 'http://ens-prod-1.ebi.ac.uk:5005/status/';
+
+my $stm = q{
+SELECT ENGINE, ROUND(SUM(DATA_LENGTH + INDEX_LENGTH), 2)/(1024*1024*1024) AS size_in_meg
+FROM information_schema.TABLES
+WHERE ENGINE IN ("InnoDB", "MyISAM")
+GROUP BY ENGINE;
+};
 
 process($_) for @ARGV;
 
 sub process {
     my $config_file = shift;
+
     local $/;
     open( my $fh, '<', $config_file );
     my $json_text = <$fh>;
+    close($fh);
+
     my $json_config = decode_json($json_text);
     my $servers = $json_config->{servers};
     print Dumper($servers);
@@ -68,45 +67,46 @@ sub process {
     warn "Processing $title\n";
 
     open my $outR, ">", "${filename_prefix}.txt";
-    print $outR join "\t", qw/server Total InnoDB Free InnoDB_free/;
+    print $outR join "\t", qw/server Free InnoDB MyISAM/;
     print $outR "\n";
 
+    my %all_stats;
+
     for my $server (@$servers) {
-        $server->{mysql_port} ||= 3306;
-        my $mysql_port = $server->{mysql_port};
-        warn "ssh-ing $server->{name}\n";
-        my $path = ($server->{path} || "/mysql/data_${mysql_port}/")."/databases/ibdata1";
-        eval {
-            @$server{qw/size avail innodb_tot/} = login($server->{name}, $path);
-            #@$server{qw/size avail innodb_tot/} = login($server->{name}, "/mysql/data_${mysql_port}/databases/ibdata1");
-        };
-        next if $@ or not defined $server->{size};
-
-        warn "querying 'information_schema' on $server->{name}\n";
-        eval {
-            my $dbh =  DBI->connect("dbi:mysql::$server->{name}:$mysql_port", "ensro", "");
-            my $sth = $dbh->prepare($stm);
-            $sth->execute('InnoDB');
-            $server->{innodb_u} = $sth->fetchrow_arrayref()->[0] || 0;
-        };
-        if ($@) {
-            $server->{innodb_u} =  $server->{innodb_tot};
-            $server->{pertableinnodb} = 1;
+        my $stats_json = get($url.$server);
+        warn "stats from $server: $stats_json\n";
+        my $stats = decode_json($stats_json);
+        $all_stats{$server} = $stats;
+        foreach my $key (qw(disk_available disk_total disk_used)) {
+            $stats->{$key} = $1*1024 if $stats->{$key} =~ /^(.*)T$/;
+            $stats->{$key} = $1/1024 if $stats->{$key} =~ /^(.*)M$/;
+            $stats->{$key} = $1 if $stats->{$key} =~ /^(.*)G$/;
         }
+        warn "querying 'information_schema' on $server\n";
+        my @size_cmd = ($server, 'batch', '', $stm);
+        local $/ = "\n";
+        open(my $size_fh, '-|', @size_cmd);
+        while (<$size_fh>) {
+            warn "Line: $_";
+            my @t = split;
+            if ($t[0] eq 'InnoDB') {
+                $stats->{innodb_used} = $t[1];
+            } else {
+                $stats->{myisam_used} = $t[1];
+            }
+        }
+        close($size_fh);
 
-        my $m = $server->{mult} || 1;
-        print $outR join "\t", ($server->{name},
-            $m * ($server->{size}),
-            $m * ($server->{pertableinnodb} ? $server->{innodb_u} : $server->{innodb_tot}),
-            $m * ($server->{avail}),
-            $m * ($server->{pertableinnodb} ? 0 : $server->{innodb_tot} - $server->{innodb_u}));
+        print $outR join "\t", ($server,
+            $stats->{disk_available},
+            $stats->{innodb_used},
+            $stats->{myisam_used});
         print $outR "\n";
-        $server->{complete} = 1;
     }
     close ($outR);
 
     # Run R and get the graph back
-    my $rcommand = "Rscript ../servers_disk.R ${filename_prefix}.txt ${filename_prefix}.png";
+    my $rcommand = "Rscript $dirname/servers_disk.R ${filename_prefix}.txt ${filename_prefix}.png";
     warn "Calling R: $rcommand\n";
     system($rcommand);
     warn "Parsing R's output\n";
@@ -115,53 +115,16 @@ sub process {
     my $div = 1024 * 1024 * 1024;
     print $outhtml sprintf($html_header, $title, $title, $currTime, $filename_prefix);
     foreach my $s (@$servers) {
-        next unless $s->{complete};
-        if ($s->{pertableinnodb}) {
+        my $stats = $all_stats{$s};
             print $outhtml sprintf($html_pertableinnodb,
-                $s->{name}, $s->{mysql_port},
-                $s->{avail}/$div,
-                $s->{innodb_u}/$div,
-                ($s->{size} - $s->{innodb_tot} - $s->{innodb_u} - $s->{avail})/$div,
+                $s,
+                $stats->{disk_available},
+                $stats->{innodb_used},
+                $stats->{myisam_used},
             );
-        } else {
-            print $outhtml sprintf($html_nopertableinnodb,
-                $s->{name}, $s->{mysql_port},
-                $s->{avail}/$div,
-                ($s->{innodb_tot} - $s->{innodb_u})/$div,
-                $s->{innodb_u}/$div,
-                ($s->{size} - $s->{innodb_tot} - $s->{avail})/$div,
-            );
-        }
     }
     print $outhtml $html_footer;
     close ($outhtml);
-}
-
-sub login {
-  my ($host, $path) = @_;
-
-  my $ibdata1_info = ssh($host, "ls -l $path\n");
-  return if $?;
-  my ($innodb) = (split/\s+/, $ibdata1_info)[4];
-
-  my ($size, $avail);
-  my $df_info = ssh($host, "df -P $path");
-  return if $?;
-  for my $df_line (split /\n/, $df_info) {
-    next if $df_line =~ /^Filesystem/;
-    my @flds = split /\s+/, $df_line;
-    if ($flds[5] =~ /mysql/) {
-      ($size, $avail) = @flds[1,3];
-    }
-  }
-  return ($size * 1024, $avail * 1024, $innodb);
-}
-
-sub ssh {
-    my ($host, $command) = @_;
-    # Default parameters: port 22, same user, no password (keys are assumed)
-    my $cmd = qq(ssh -o StrictHostKeyChecking=no $host $command);
-    return `$cmd`;
 }
 
 
