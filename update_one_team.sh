c5471d1c25db97c2fc3a3e4fa86a594a73fdb6d4
Matthieu Muffato <muffato@ebi.ac.uk>
2017-04-13 11:52:56 +0100

    Added a script to update a single team

---
+++ update_one_team.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# Copyright [2016-2017] EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd ~/workspace/disk/cron_ebi/
+log_name=`date --rfc-3339=seconds | sed 's/ /T/'`
+(
+    perl ../servers_disk.pl ../${1}.json
+    /usr/bin/cp -p *png *html $HOME/public_html/disk/
+) > log/$log_name.out 2> log/$log_name.err
+
